b67d3c234fb38ac7d304044a0f9eca075d44a517
Anton Mukhataev <miksartono@proton.me>
2025-03-09 17:44:40 +0100

    Changed: light style and block edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,7 +38,7 @@ module.exports = {
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   darkMode: ['selector'],
   daisyui: {
-    themes: ['emerald', 'forest'],
-    darkTheme: 'forest',
+    themes: ['valentine', 'synthwave'],
+    darkTheme: 'synthwave',
   },
 }
